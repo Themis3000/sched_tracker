c02d2fa3c588560f85837a9ada8b240746334d30
Themi <Tcm4760@gmail.com>
2020-09-17 12:08:30 -0500

    -set to show when first hour will be starting

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # Sched tracker
 A time tracker to help you keep track of your school's schedule, easily configured by a json file
 and used by others
+
+Config file is set to work for the 2020-2021 southwest school year and is currently hosted at https://themis3000.github.io/sched_tracker/
--- script.js
+++ script.js
@@ -11,14 +11,19 @@ function class_update_loop() {
     let sched_info = get_sched_info();
     if (sched_info.activity !== undefined) {
         $("#activity").text(sched_info.activity[0]);
-        timer_update_loop(sched_info);
+        timer_update_loop(sched_info.activity);
+    } else if (sched_info.next_activity !== undefined) {
+        $("#activity").text(sched_info.next_activity[0] + " starting in");
+        timer_update_loop(sched_info.next_activity);
+    } else {
+        setTimeout(class_update_loop, 10000);
     }
 }
 
-function timer_update_loop(sched_info) {
+function timer_update_loop(activity) {
     let current_date = new Date();
     let midnight = new Date().setHours(0, 0, 0, 0);
-    let end_date = new Date(midnight + (sched_info.activity[2] * 1000));
+    let end_date = new Date(midnight + (activity[2] * 1000));
     let ends_in_sec = Math.ceil((end_date - current_date) / 1000);
     if (ends_in_sec <= 0) {
         $("#timer").text('');
@@ -38,7 +43,7 @@ function timer_update_loop(sched_info) {
             $("#timer").text(hours_left + " hours left");
         }
     }
-    setTimeout(function() {timer_update_loop(sched_info)}, 1000);
+    setTimeout(function() {timer_update_loop(activity)}, 1000);
 }
 
 function get_sched_info() {
@@ -50,7 +55,8 @@ function get_sched_info() {
     let activity;
     let week;
     let day;
-    // Check if school in ongoing
+    let next_activity;
+    // Check if school is ongoing
     if (sched["active_epoch_range"][0] <= Date.now() / 1000 <= sched["active_epoch_range"][1]) {
         // Check if the day is a special day off
         if (!(day_of_year in sched["day_off_exclusions"])) {
@@ -63,11 +69,15 @@ function get_sched_info() {
             // gets the current day of the week
             day = sched["days"][week[date.getDay()]];
             // gets current hour, if any
-            for (let hour of day) {
+            for (const [index, hour] of day.entries()) {
                 if (hour[1] <= seconds_since_midnight && seconds_since_midnight <= hour[2]) {
                     activity = hour;
+                    next_activity = day[index + 1];
                     break;
                 }
+            }
+            if (day[0][1] > seconds_since_midnight) {
+                next_activity = day[0];
             }
         }
     } else {
@@ -76,7 +86,8 @@ function get_sched_info() {
     }
     return {
         during_year: during_year,
-        activity: activity
+        activity: activity,
+        next_activity: next_activity
     };
 }
 
